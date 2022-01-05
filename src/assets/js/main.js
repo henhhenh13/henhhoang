bb6e6069d6f6afe2fbfbda9384374ea4ef9791d8
henhhenh13 <74145131+henhhenh13@users.noreply.github.com>
2022-01-05 12:08:39 +0700

    update

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,10 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const links = $$('.link');
+const boxImg = $('#boxImg');
+const boxLine = $('#boxLine');
+
+
 
 links.forEach(item => {
     item.onclick = function () {
@@ -11,3 +15,14 @@ links.forEach(item => {
         this.classList.add('active')
     }
 })
+
+const handleMouseOverW = (e) => {
+    
+    boxImg.style.transform = `translate3d(-${e.clientX / 50}px, -${e.clientY / 50}px, 0px)`;
+    // boxLine.style.transform = `translate(${e.clientX / 80}px, ${e.clientY / 80}px)`;
+    
+    
+   
+}
+
+window.addEventListener("mousemove", handleMouseOverW)
